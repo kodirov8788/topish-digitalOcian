dc4f19f3ea88173ea51000531b95d7e675711f28
kodirov8788 <kodirov8788@gmail.com>
2024-07-29 12:03:38 +0500

    fixed socket 18

--- src/socket/socketHandlers.js
+++ src/socket/socketHandlers.js
@@ -364,6 +364,7 @@ const handleSendMessage = async (socket, { text, recipientId, senderId, files },
         if (recipient && recipient.socketId) {
             console.log("recipient.socketId:=>", recipient.socketId);
             io.to(recipient.socketId).emit("getMessage", messageToSend);
+            io.to(recipient.socketId).emit("getMessageToOther", messageToSend);
         }
 
         // Always emit the message to the sender
